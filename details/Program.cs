bc7aa79a9e0a6fb59daabf0516fb4cde65fcac15
Nicholas Coxen <nicjcoxen@gmail.com>
2022-01-01 22:56:20 +1300

    Add project files.

---
+++ details/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Login
+{
+    class Username_Password
+    {
+        public static void Main()
+        {
+            string username, password;
+            Console.Write("Enter username: ");
+            username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
+
+            if (username == "Nic" && password == "apv45qgy")
+            {
+                Console.WriteLine("Access Granted.");
+            }
+            else if (username != "Nic" && password == "apv45qgy")
+            {
+                Console.WriteLine("The username is wrong.");
+            }
+            else if (username == "Nic" && password != "apv45qgy")
+            {
+                Console.WriteLine("The password is wrong.");
+            }
+            else
+            {
+                Console.WriteLine("Access Denied.");
+            }
+        }
+    }
+}
